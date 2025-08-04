5cf6b8cbc8ea44c18fb493b5c76fe518221fdd49
Gabriel Jaeger <gabriel.jaeger@oneweb.com.br>
2025-08-04 13:52:40 -0300

    :sparkles: feat(usecases): add ListUsersUseCase and enhance AuthenticateUseCase by including sessionService

---
+++ src/core/use_cases/ListUsersUseCase.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace src\core\use_cases;
+
+use src\core\repositories\UsersRepositoryInterface;
+use src\core\repositories\requests\UserSearchRequest;
+use src\core\entities\User;
+
+class ListUsersUseCase
+{
+  private UsersRepositoryInterface $usersRepository;
+
+  public function __construct(UsersRepositoryInterface $usersRepository)
+  {
+    $this->usersRepository = $usersRepository;
+  }
+
+  public function execute(int $page, string $name): array
+  {
+    $users = $this->usersRepository->list(new UserSearchRequest(page: $page, login: $name));
+
+    return $users;
+  }
+}
--- src/infra/container/usecases.php
+++ src/infra/container/usecases.php
@@ -1,8 +1,18 @@
 <?php
 use src\core\use_cases\AuthenticateUseCase;
+use src\core\use_cases\ListUsersUseCase;
+
 
 return [
   'authenticateUseCase' => function ($c) {
-    return new AuthenticateUseCase($c->get('usersRepository'));
+    return new AuthenticateUseCase(
+      $c->get('usersRepository'),
+      $c->get('sessionService')
+    );
   },
+  'listUsersUseCase' => function ($c) {
+    return new ListUsersUseCase(
+      $c->get('usersRepository')
+    );
+  }
 ];
